cb0bc383c33c9fe9bc9178b135451a6ea5ad00cd
Eduardo Jesus TorresPerez <torrespe@mail.lcc.edu>
2018-08-28 13:55:08 -0400

    First Commit

---
+++ test/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+/* CPSC 230TR
+ * Practice Lab
+ * Rodger
+ * Matt
+ * This Program will greet the hello world and show our input
+ *
+ * */
+
+// Main Function
+int main() {
+    // algorithm
+    // Get number of bottles
+    // Get profit per bottle
+    // get number of days
+    // multiply profit per bottle by number of bottles per day
+    // Display
+
+
+    // Variables profitPerBottle, totalProfitYear, ProfitPerDay, numBottles
+
+
+
+    return 0;
+}
+
