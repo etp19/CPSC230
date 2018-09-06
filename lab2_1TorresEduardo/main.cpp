f55b98fcaa458d18fcb4a3fcb9282183c0e18498
Eduardo Jesus TorresPerez <torrespe@mail.lcc.edu>
2018-09-06 14:02:05 -0400

    change 100 for 10

--- lab2_1TorresEduardo/main.cpp
+++ lab2_1TorresEduardo/main.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 // Constant
-double const MPH2MPS = (1609.0/3600.0);
+const double MPH2MPS = (1609.0/3600.0);
 int main() {
 
     // Variables
@@ -17,24 +17,24 @@ int main() {
     int newAcceleration;
 
     // Input
-    cout << "ease enter the velocity in miles per hour:";
+    cout << "Please enter the velocity in miles per hour:";
     cin >> velocity;
     cout << "\n";
-    cout << "ease enter the time in seconds:";
+    cout << "Please enter the time in seconds:";
     cin >> time;
 
     // Calculation
     acceleration = (MPH2MPS) * (velocity/time);
 
     // Rounding
-    acceleration *= 100;
+    acceleration *= 10;
     acceleration += 0.5;
     newAcceleration = static_cast<int>(acceleration);
 
-    acceleration = static_cast<double>(newAcceleration)/100;
+    acceleration = static_cast<double>(newAcceleration)/10;
 
     //Output
-    cout << "The acceleration required by a vehicle to reach a velocity of 60.00 miles per hour in 6.00 seconds is 4.50 meters per second";
+    cout << "The acceleration required by a vehicle to reach a velocity of  " << velocity << " miles per hour in  " << time << "  seconds is  " << acceleration <<"  meters per second";
 
     return 0;
 }
