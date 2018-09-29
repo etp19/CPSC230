df708681e124f95b39b671351ffb80b421310c40
Eduardo Torres Perez <torrespe@mail.lcc.edu>
2018-09-28 23:47:02 -0400

    added few features and move things around

--- hw ch2/hw2ETP.cpp
+++ hw ch2/hw2ETP.cpp
@@ -1,6 +1,7 @@
 /*******************************************************************************************************************************************
 *                      CPSC 230 TR Fall 2018               HW Ch02
 * Eduardo Torres
+* Gregory
 *
 * The program allows the user to guess a number 5 times and after each guess, it will display whether or not the user guessed the number.
 *
@@ -11,17 +12,14 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <thread>
 
 using namespace std;
+using namespace this_thread;
+using namespace chrono;
 
 // Constant
-enum WEAPONS {
-	A = 1,
-	B = 2,
-	C = 3,
-	D = 4
-};
-
 enum WEAPONS_DAMAGE {
 	SWORD = 5000,
 	WAND = 6000,
@@ -29,6 +27,7 @@ enum WEAPONS_DAMAGE {
 	RING = 8000
 };
 int const TIMES_TO_RUN = 5;
+string const sword = "Sword of Greyskull";
 int main()
 {	
 
@@ -37,16 +36,16 @@ int main()
 		
 	// Variables
 	int counter = 0;
-	string userName, playAgain = "yes";
-	int weaponType, weaponDamage, monsterPoints, initialUserPoints;
+	string userName;
+	char playAgain = 'y';
+	int weaponType, monsterPoints, initialUserPoints;
 	
-
 	//Input your name
 	cout << "Please enter your name:  ";
 	cin >> userName;
 	cout << "\n";
 	
-	while(playAgain == "yes"){
+	while(playAgain == 'y'){
 	// random points to user
 	initialUserPoints = (rand() % 50000) + 1;
 	// random points to monster
@@ -57,55 +56,59 @@ int main()
 
 	cout << "You have " << initialUserPoints << "  Points\n";
 
-	// Choose your weapon
-	cout << "/n Choose a weapon from the following:\n\n";
-	cout << "/n1 = Sword of Greyskull/n";
-	cout << "/n2 = Wand of Gandolf/n";
-	cout << "/n3 = Battle Axe of Grumpy/n";
-	cout << "/n4 = Ring of Gollum/n";
-	cin >> weaponType;
 
-	
-
-	switch (weaponType)
+	for (int i = 1; i <= TIMES_TO_RUN; i++)
 	{
-	case A:
-		cout << "You choose weapon A";
-		weaponDamage = SWORD;
-		initialUserPoints += weaponDamage;
-	case B:
-		cout << "You choose weapon B";
-		weaponDamage = WAND;
-		initialUserPoints += weaponDamage;
-	case C:
-		cout << "You choose weapon C";
-		weaponDamage = BATTLE;
-		initialUserPoints += weaponDamage;
-	case D:
-		cout << "You choose weapon D";
-		weaponDamage = RING;
-		initialUserPoints += weaponDamage;
-	}
+		cout << "\nTry #  " << i << endl;
+		// Choose your weapon
+		cout << "\n ****** Choose a weapon from the following menu *******" << endl;
+		cout << "1- Sword of Greyskull" << endl;
+		cout << "2- Wand of Gandolf" << endl;
+		cout << "3- Battle Axe of Grumpy" << endl;
+		cout << "4- Ring of Gollum" << endl;
+		cout << "Any key to exit the menu" << endl;
+		cin >> weaponType;
 
-	
 
-	for (int i = 0; i < TIMES_TO_RUN; i++)
-	{
-		if (initialUserPoints < monsterPoints) {
+		switch (weaponType)
+		{
+		case 1:
+			initialUserPoints += SWORD;
+			break;
+		case 2:
+			initialUserPoints += WAND;
+			break;
+		case 3:
+			initialUserPoints += BATTLE;
+			break;
+		case 4:
+			initialUserPoints += RING;
+			break;
+		default:
+			cout << "No weapons was chosen, the game is ending ...." << endl;
+			sleep_until(system_clock::now() + seconds(1));
+			i = TIMES_TO_RUN + 1;
+			continue;
+			break;
+		}
+		cout <<	"																					************* Game Board ************" << endl;
+		cout << "																					**  Monster points:  " << monsterPoints << "  **" << endl;
+		cout << "																					**  User points:  "	   << initialUserPoints << "  **" << endl;
+		cout << "You chose the weapon  " << weaponType << endl;
+		if (initialUserPoints > monsterPoints) {
 			initialUserPoints += (initialUserPoints * 0.15);
 			monsterPoints += (initialUserPoints * 0.20);
-			cout << "With the weapon you chose you have defeated the Monster\n";
+			cout << "With the weapon  " << weaponType << "  you have defeated the Monster" << endl;
 		}
 		else
 		{	
 			initialUserPoints -= (initialUserPoints * 0.15);
-			monsterPoints -= (initialUserPoints * 0.20);
-			cout << "With the weapon you chose you have lost\n";
+			cout << "With the weapon  " << weaponType << "  you have lost" << endl;
 		}
 	}
 
 
-	cout << "Do You want to play again?  \n";
+	cout << "Do You want to play again? Enter y/n" << endl;
 	cin >> playAgain;
 	
 	}
