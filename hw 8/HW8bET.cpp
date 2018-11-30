9debe232acba6922c8bd248bd8106f66270ba9c9
Eduardo Torres Perez <torrespe@mail.lcc.edu>
2018-11-29 20:22:49 -0500

    added hw8b

---
+++ hw 8/HW8bET.cpp
@@ -0,0 +1,23 @@
+/*****************************************************************************
+// CPSC 230			Fall 2018			HW # 8b
+// Eduardo Torres
+// The program display a list of phone numbers, you can display each separately,
+// as well as edit them or create new ones. 
+//
+****************************************************************************/
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int x = 7;
+	int *ptr = &x;
+	cout << *ptr;
+
+	system("pause");
+}
